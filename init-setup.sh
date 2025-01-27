93143ac1820e61b96ebee55fa3f33ff49005b4aa
ThatsNeat <john.rohweder1015@gmail.com>
2025-01-26 22:09:31 -0800

    updated and made better

--- init-setup.sh
+++ init-setup.sh
@@ -1,61 +1,57 @@
 #!/bin/bash
 
 # keyboard and time setup
-loadkeys us
-timedatectl set-timezone America/Los_Angeles
+echo "Setting keyboard and timedate..."
+loadkeys us &>/dev/null
+timedatectl set-timezone America/Los_Angeles &>/dev/null
 
 # partitions
-echo -e "g\nn\n\n\n+4G\nn\n\n\n+8G\nn\n\n\n\nt\n2\n19\nt\n3\n23\nt\n1\n1\nw\n" | fdisk /dev/sda
+echo "Partitioning Drive..."
+parted -s /dev/sda \
+  mklabel gpt \
+  mkpart ESP fat32 1MiB 4096MiB \
+  set 1 esp on \
+  mkpart primary linux-swap 4096MiB 12288MiB \
+  mkpart primary ext4 12288MiB 100% \  &>/dev/null
 
 # formatting new partitions
-mkfs.ext4 /dev/sda3
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
+echo "Formatting Drive..."
+mkfs.fat -F32 /dev/sda1 &>/dev/null
+mkswap /dev/sda2 && swapon /dev/sda2 &>/dev/null
+mkfs.ext4 /dev/sda3 &>/dev/null
 
 # mounting new partitions
+echo "Mounting New Partitions..."
+umount /mnt
 mount /dev/sda3 /mnt
 mount --mkdir /dev/sda1 /mnt/boot/efi
-swapon /dev/sda2
 
 # installing needed packages
+echo "Installing Base Packages" &>/dev/null
 pacstrap -i /mnt base
 
 # generating ftab
+echo "Generating fstab"
 genfstab -U -p /mnt >>/mnt/etc/fstab
 
-# switch to chroot
+# system configure
+echo "Configuring System..."
 arch-chroot /mnt /bin/bash <<EOF
-# setting time
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-hwclock --systohc
+  # setting time
+  ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime &> /dev/null
+  hwclock --systohc &> /dev/null
 
-# setting locale
-echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
-locale-gen
+  # setting locale
+  echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+  locale-gen &> /dev/null
 
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
-echo "KEYMAP=us" >/etc/vconsole.conf
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  echo "KEYMAP=us" > /etc/vconsole.conf
 
-# package install
-pacman -S --noconfirm neovim hyprland base-devel grub networkmanager sudo man linux linux-headers linux-firmware
+  # package install
+  pacman -S --noconfirm neovim hyprland base-devel grub networkmanager sudo man linux linux-headers linux-firmware
 
-# install intel/nvidia stuff later
-
-# linux kernel stuff
-mkinitcpio -p linux
-
-# grub install
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# start networkmanager
-systemctl enable NetworkManager
-
-# CREATE A NEW USER MANUALLY
-# passwd
-# useradd -m -g users -G wheel johnathon
-# passwd johnathon
+  # grub install
+  grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck &> /dev/null
+  grub-mkconfig -o /boot/grub/grub.cfg &> /dev/null
 EOF
-
-# umount -a
