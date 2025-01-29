38f4057341c8d61d100e198bb897136197ae6659
ThatsNeat <john.rohweder1015@gmail.com>
2025-01-29 12:21:41 -0800

    added steps for later use

--- automated/setup.sh
+++ automated/setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# for the setup of graphics and such when within hyprland
+# setup
+# downloaded neovim
+# downloaded neofetch
+# downloaded git
+# git clone https://aur.archlinux.org/nerd-fonts-fira-code.git
+# then makepkg -si
+# fc-cache -fv
+# waybar
+# downloaded man
+# waybar in /etc/xdg/waybar
+#
+
+# todo
+# fix neovim syntax highlighting
